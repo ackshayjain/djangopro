dd8ef60f12963451fb6d85e55fc3a856ca6334a4
Ackshay <ackshayjain@gmail.com>
2018-04-03 14:27:00 +0530

    testing index

--- ackshay/settings/base.py
+++ ackshay/settings/base.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'ackshay.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '.../Templates')],
+        'DIRS': [os.path.join(BASE_DIR, './Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
