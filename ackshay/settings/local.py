d172b04cb6d02651d0cfb0529fae5cfbd0d9174d
Ackshay <ackshayjain@gmail.com>
2018-04-03 11:53:30 +0530

    testing index

--- ackshay/settings/local.py
+++ ackshay/settings/local.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'ackshay.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [os.path.join(BASE_DIR, "Templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,9 +71,7 @@ TEMPLATES = [
     },
 ]
 
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, '../Templates'),
-)
+
 
 WSGI_APPLICATION = 'ackshay.wsgi.application'
 
--- ackshay/settings/production.py
+++ ackshay/settings/production.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'ackshay.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [os.path.join(BASE_DIR, "Templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,9 +73,6 @@ TEMPLATES = [
         },
     },
 ]
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, '../Templates'),
-)
 
 WSGI_APPLICATION = 'ackshay.wsgi.application'
 
