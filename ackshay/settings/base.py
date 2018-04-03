865e6f859383d808b3b37daff53c3084e8126df0
Ackshay <ackshayjain@gmail.com>
2018-04-03 11:51:44 +0530

    'templatess'

--- ackshay/settings/base.py
+++ ackshay/settings/base.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'ackshay.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,10 +69,6 @@ TEMPLATES = [
         },
     },
 ]
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, '../Templates'),
-)
 
 WSGI_APPLICATION = 'ackshay.wsgi.application'
 
